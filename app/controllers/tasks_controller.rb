bfee70e95c0219f00932b8aa93009541166297ea
Niels van Klooster <nielsvanklooster@gmail.com>
2018-07-31 17:36:20 +0800

    got the index and details working. Adding/editing/deleting not yet

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,40 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params[:task])
+    @task.save
+
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task = Tasks.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
 end
